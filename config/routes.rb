2aa8f5b1ce265200e14835047690271e589a5e98
JoelLindow <joellindow@gmail.com>
2017-11-19 20:48:19 -0700

    filthy coding done with no testing. can now create articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  resources :articles, only: [:index, :show, :create]
+
+  get '/producestories', to: 'articles#new'
+
+
+  #SUPERUSER FOR HEROKU IS 'james'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(
+  title: "Conspiracy Story Number 1",
+  author_name: "Mr. Secret Author Pants",
+  main_img_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+  paragraph_one: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  paragraph_two: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  paragraph_three: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  evidence_img_one_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+  evidence_img_two_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+)
+Article.create(
+  title: "Conspiracy Story Number 2",
+  author_name: "Mr. Secret Author Pants",
+  main_img_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+  paragraph_one: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  paragraph_two: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  paragraph_three: "Lorem ipsum dolor sit amet, non pellentesque vel. Tellus eget massa, blandit vel quis elit nulla sed, nec bibendum egestas mollis leo commodo cras. Nec elit, accumsan vivamus vehicula ipsum sem nisi, libero duis eligendi aenean pellentesque mi, duis at pretium enim. Sit condimentum eu arcu inventore, diam in sit egestas, ornare nunc malesuada, pede at porttitor suspendisse. Id elit donec condimentum ac, morbi nec cras sem quam mollis, felis sit erat vivamus elementum pellentesque. Ac sodales, ut rutrum.",
+  evidence_img_one_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+  evidence_img_two_url: "https://www.martinezcreativegroup.com/wp-content/uploads/2014/05/img-placeholder.png",
+)
